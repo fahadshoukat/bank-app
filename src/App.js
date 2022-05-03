5e26ae8b1c474dbaab040d2d1e2c55a483a825bc
fahadshoukat <fraza6970@gmail.com>
2022-05-03 23:03:14 +0500

    add material ui appbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import ResponsiveAppBar from './components/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <ResponsiveAppBar />
     </div>
   );
 }
