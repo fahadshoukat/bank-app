2ad11aaf24dea15048bbb215cade19537fbbd6ed
fahadshoukat <fraza6970@gmail.com>
2022-05-04 21:31:31 +0500

    delete components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import ResponsiveAppBar from './components/NavBar';
-import BasicCard from './components/AccountPaper';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <ResponsiveAppBar />
-     <BasicCard />
+     
     </div>
   );
 }
