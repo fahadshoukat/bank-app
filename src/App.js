605c414379ac2254a636263f29c8142a69af97bb
fahadshoukat <fraza6970@gmail.com>
2022-05-04 21:13:10 +0500

    add cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import ResponsiveAppBar from './components/NavBar';
+import BasicCard from './components/AccountPaper';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <ResponsiveAppBar />
+     <BasicCard />
     </div>
   );
 }
