b9bbe251468520863571b198e8c763eb42f84e45
fahadshoukat <fraza6970@gmail.com>
2022-05-06 22:13:39 +0500

    add create account form

--- src/components/createAcount/CreateAccount.js
+++ src/components/createAcount/CreateAccount.js
@@ -1,93 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateAccount = () => {
+
+  const [users, setUsers] = useState({});
+
+  function handleSubmit(event){
+    event.preventDefault();
+    
+  }
   return (
-    <form class="row g-3">
-      <div class="col-md-4">
-        <label for="validationDefault01" class="form-label">
-          First name
-        </label>
-        <input
-          type="text"
-          class="form-control"
-          id="validationDefault01"
-          placeholder="Enter First Name"
-          required
-        />
-      </div>
-      <div class="col-md-4">
-        <label for="validationDefault02" class="form-label">
-          Last name
-        </label>
-        <input
-          type="text"
-          class="form-control"
-          id="validationDefault02"
-          placeholder="Enter Last Name"
-          required
-        />
-      </div>
-      <div class="col-md-4">
-        <label for="validationDefaultUsername" class="form-label">
-          CNIC
-        </label>
-        <div class="input-group has-validation">
-          <input
-            type="number"
-            class="form-control"
-            id="validationDefaultUsername"
-            placeholder="XXXXX-XXXXXXX-X"
-            pattern="[0-9]{5}-[0-9]{7}-[0-9]{1}"
-            required
-          />
+    <div className="container">
+      <div className="row mt-5">
+        <div className="col col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+          <form className="row g-3">
+            <div className="bg-primary text-white p-3"> 
+            <h3 className="text-center">Enter Account Details</h3>
+            <p className="text-center m-0">All fields are required<span className="text-danger">*</span></p>
+            </div>
+            <div className="col-md-6 p-2">
+              <input
+                type="text"
+                className="form-control"
+                id="validationDefault01"
+                placeholder="Enter Full Name"
+                required
+              />
+            </div>
+            <div className="col-md-6  p-2">
+              <input
+                type="number"
+                className="form-control"
+                id="validationDefault02"
+                placeholder="Enter CNIC XXXXXXXXXXXXX"
+                size={13}
+                required
+              />
+            </div>
+            <div className="col-md-6  p-2">
+              <div className="input-group">
+                <input
+                  type="number"
+                  className="form-control"
+                  id="validationDefaultUsername"
+                  aria-describedby="inputGroupPrepend2"
+                  placeholder="Enter Branch Code (1-99)"
+                  min={1}
+                  max={99}
+                  required
+                />
+              </div>
+            </div>
+            <div className="col-md-6 p-2">
+              <input
+                type="text"
+                className="form-control"
+                id="validationDefault03"
+                placeholder="Enter Account Number (Length must be 9)"
+                size={9}
+                required
+              />
+            </div>
+            <div className="col-md-6 p-2">
+              <select className="form-select" id="validationDefault04" required>
+                <option defaultValue='Choose Account Type'>
+                  Choose Account Type
+                </option>
+                <option>Current</option>
+                <option>Saving</option>
+              </select>
+            </div>
+            <div className="col-md-6 p-2">
+              <input
+                type="text"
+                className="form-control"
+                id="validationDefault05"
+                placeholder="Initial Deposit(Minimum RS/500.)"
+                required
+              />
+            </div>
+            <div className="col-12 text-end p-2">
+              <button onClick={handleSubmit} className="btn btn-success" type="submit">
+                Create Account
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-      <div class="col-md-6">
-        <label for="validationDefault03" class="form-label">
-          Account Number
-        </label>
-        <input
-          type="number"
-          class="form-control"
-          id="validationDefault03"
-          size={8}
-          required
-        />
-      </div>
-      <div class="col-md-3">
-        <label for="validationDefault04" class="form-label">
-          Account Type
-        </label>
-        <select
-          class="form-select"
-          id="validationDefault04"
-          required
-        >
-          <option selected disabled value="">
-            Choose Type
-          </option>
-          <option>Current</option>
-          <option>Saving</option>
-        </select>
-      </div>
-      <div class="col-md-3">
-        <label for="validationDefault05" class="form-label">
-          Amount
-        </label>
-        <input
-          type="text"
-          class="form-control"
-          id="validationDefault05"
-          min={3}
-          required
-        />
-      </div>
-      <div class="col-12">
-        <button class="btn btn-primary" type="submit">
-          Submit form
-        </button>
-      </div>
-    </form>
+    </div>
   );
 };
 
