f1541ca703c0345f394404a5096ae447e8e05ba8
fahadshoukat <fraza6970@gmail.com>
2022-05-08 23:33:10 +0500

    get form data and store it

--- src/components/createAcount/CreateAccount.js
+++ src/components/createAcount/CreateAccount.js
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const CreateAccount = () => {
 
-  const [users, setUsers] = useState({});
+  const navigate = useNavigate();
 
-  function handleSubmit(event){
-    event.preventDefault();
-    
+  const [users, setUsers] = useState({
+    name: '', cnic: '', branchCode: '', accountNumber: '', accountType: '', deposit: '',
+  });
+
+  let name, value
+
+  function handleInputs(e){
+    name = e.target.name
+    value = e.target.value
+    setUsers({...users, [name]: value})
+  }
+  function handleSubmit(e){
+    e.preventDefault();
+    navigate('/accounts');
   }
   return (
     <div className="container">
       <div className="row mt-5">
         <div className="col col-sm-8 offset-sm-2 col-md-6 offset-md-3">
-          <form className="row g-0">
+          <form className="row" onSubmit={handleSubmit}>
             <div className="bg-primary text-white p-3"> 
             <h3 className="text-center">Enter Account Details</h3>
             <p className="text-center m-0">All fields are required<span className="text-danger">*</span></p>
@@ -20,6 +32,9 @@ const CreateAccount = () => {
             <div className="col-md-6 p-2">
               <input
                 type="text"
+                name="name"
+                value={users.name}
+                onChange={handleInputs}
                 className="form-control"
                 id="validationDefault01"
                 placeholder="Enter Full Name"
@@ -29,9 +44,14 @@ const CreateAccount = () => {
             <div className="col-md-6  p-2">
               <input
                 type="number"
+                name="cnic"
+                value={users.cnic}
+                onChange={handleInputs}
                 className="form-control"
                 id="validationDefault02"
-                placeholder="Enter CNIC XXXXXXXXXXXXX"
+                placeholder="Enter CNIC without dashes"
+                minLength='13'
+                maxLength='13'
                 required
               />
             </div>
@@ -39,6 +59,9 @@ const CreateAccount = () => {
               <div className="input-group">
                 <input
                   type="number"
+                  name="branchCode"
+                  value={users.branchCode}
+                  onChange={handleInputs}
                   className="form-control"
                   id="validationDefaultUsername"
                   aria-describedby="inputGroupPrepend2"
@@ -52,15 +75,23 @@ const CreateAccount = () => {
             <div className="col-md-6 p-2">
               <input
                 type="text"
+                name="accountNumber"
+                value={users.accountNumber}
+                onChange={handleInputs}
                 className="form-control"
                 id="validationDefault03"
-                placeholder="Enter Account Number (Length must be 9)"
-                size={9}
+                placeholder="Account Number (Length must be 9)"
+                minLength='9'
+                maxLength='9'
                 required
               />
             </div>
             <div className="col-md-6 p-2">
-              <select className="form-select" id="validationDefault04" required>
+              <select className="form-select" id="validationDefault04" 
+              name="accountType"
+              value={users.accountType}
+              onChange={handleInputs}
+              required>
                 <option defaultValue='Choose Account Type'>
                   Choose Account Type
                 </option>
@@ -71,6 +102,9 @@ const CreateAccount = () => {
             <div className="col-md-6 p-2">
               <input
                 type="text"
+                name="deposit"
+                value={users.deposit}
+                onChange={handleInputs}
                 className="form-control"
                 id="validationDefault05"
                 placeholder="Initial Deposit(Minimum RS/500.)"
@@ -78,7 +112,7 @@ const CreateAccount = () => {
               />
             </div>
             <div className="col-12 text-end p-2">
-              <button onClick={handleSubmit} className="btn btn-success" type="submit">
+              <button className="btn btn-success" type="submit">
                 Create Account
               </button>
             </div>
