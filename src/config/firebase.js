e86419eab03c56fd5d5d8f70af1889e8a0440df5
fahadshoukat <fraza6970@gmail.com>
2022-05-20 21:45:03 +0500

    create firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCLNhNM7GJAc1FyJ4nM140g1bZB5pG1hHg",
+  authDomain: "my-bank-app-70.firebaseapp.com",
+  databaseURL: "https://my-bank-app-70-default-rtdb.firebaseio.com",
+  projectId: "my-bank-app-70",
+  storageBucket: "my-bank-app-70.appspot.com",
+  messagingSenderId: "815962329563",
+  appId: "1:815962329563:web:57e769335d5fe0afe617fe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
